a5d597c98fb8df55431d92aa81f2386cf5e54f11
yeonloca <88708703+yeonloca@users.noreply.github.com>
2021-08-10 18:00:28 +0900

    Add files via upload

---
+++ kim 0810/구구단.py
@@ -0,0 +1,11 @@
+r = int(input("몇번 반복할까요?"))
+
+while True
+    x = int(input("x :"))
+    y = int(input("y :"))
+    n = int(input("x + y ="))
+
+    if x+y == n:
+        print("맞았습니다.")
+    else:
+        print("틀렸습니다.")
